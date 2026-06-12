9c9bafda0495e90c52a841da7929613778d25ed9
yanlucasyjj-svg <yan.lucas.yjj@gmail.com>
2026-06-12 11:53:44 -0300

    Add files via upload

---
+++ zercíciosJS_C/Multiplicação/script.js
@@ -0,0 +1,6 @@
+function mult(){
+    var n1 = document.getElementById("n1").valueAsNumber
+    var n2 = document.getElementById("n2").valueAsNumber 
+    var resp = document.getElementById("respMult")
+    resp.textContent = n1 * n2
+}
