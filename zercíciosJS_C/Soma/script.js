a4fe1bb9c7efc7ef8082002328578f6eb92f50be
yanlucasyjj-svg <yan.lucas.yjj@gmail.com>
2026-06-12 09:28:54 -0300

    Add files via upload

---
+++ zercíciosJS_C/Soma/script.js
@@ -0,0 +1,6 @@
+function somar(){
+    var n1 = document.getElementById("n1").valueAsNumber
+    var n2 = document.getElementById("n2").valueAsNumber 
+    var resp = document.getElementById("resp")
+    resp.textContent = n1 + n2
+}
---
+++ zercíciosJS_C/Subtração/script.js
@@ -0,0 +1,6 @@
+function sub(){
+    var n1 = document.getElementById("n1").valueAsNumber
+    var n2 = document.getElementById("n2").valueAsNumber 
+    var resp = document.getElementById("respSub")
+    resp.textContent = n1 - n2
+}
