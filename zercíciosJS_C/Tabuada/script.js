86fe1c53123a5c9e135f807fc1d2f57148a6a3c9
yan-yjj <yan.lucas.yjj@gmail.com>
2026-06-15 11:43:55 -0300

    Add files via upload

--- zercíciosJS_C/Tabuada/script.js
+++ zercíciosJS_C/Tabuada/script.js
@@ -2,14 +2,14 @@ function tabuada(){
     var num = document.getElementById("n1").valueAsNumber
     var i = 0
 
-    var texto = "T A B U A D A\n";
-    texto += "==============\n";
+    var texto = "T A B U A D A<br>";
+    texto += "==============<br>";
 
     for (var i = 0; i <= 10; i++) {
-        texto += `${n} x ${i} = ${n * i}\n`;
+        texto += `${num} x ${i} = ${num * i}<br>`;
     }
 
     texto += "==============";
 
-    resp.textContent = texto;
+    resp.innerHTML = texto;
 }
---
+++ zercíciosJS_C/Texto/script.js
@@ -0,0 +1,6 @@
+function texto(){
+    var t1 = document.getElementById("t").value
+    var resp = document.getElementById("resp")
+
+    resp.textContent = t1
+}
